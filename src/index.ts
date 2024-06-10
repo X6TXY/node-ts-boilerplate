ca31c3b7685ec65268515965b9238d10cfa98e94
effuone <albkfil@gmail.com>
2024-06-10 20:31:20 +0500

    fixed index.ts git logs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import express, { Request, Response } from 'express';
 import { logger } from './logger';
-<<<<<<< HEAD
 import globalRouter from './global-router';
 import 'dotenv/config';
 
-=======
->>>>>>> upstream/main
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,13 +10,9 @@ app.use(logger);
 app.use(express.json());
 app.use(globalRouter);
 
-<<<<<<< HEAD
 app.use('/api/users', (req: Request, res: Response) => {
   res.send('Hello, world');
 });
-=======
-
->>>>>>> upstream/main
 
 app.listen(PORT, () => {
   console.log(`Server runs at http://localhost:${PORT}`);
