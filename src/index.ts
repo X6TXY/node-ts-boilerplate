87cb8408a5fec9f7a7884300640e1aba00d27c96
Toleu Bahauddin <baha200477@gmail.com>
2024-06-10 09:48:36 +0500

    add readme and logger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import express, { NextFunction } from 'express';
+import express from 'express';
+import { logger } from './logger';
 import UsersRouter from './routes/users';
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+app.use(logger);
 
 app.use('/api/users', UsersRouter);
 
-app.listen(PORT,()=>{
-    console.log('listening on port '+PORT);
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
 });
---
+++ src/logger.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    res.on('finish', () => {
+        console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} ${res.statusCode}`);
+    });
+    next();
+};
+
+export { logger };
